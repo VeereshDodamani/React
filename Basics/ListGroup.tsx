b5a08581e0dd3c2a447c1803a5bd5cc6b3a8b869
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-08-06 22:44:47 +0530

    Create ListGroup.tsx

---
+++ Basics/ListGroup.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+interface Props{
+  items : string[];
+  heading : string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({items,heading,onSelectItem}:Props){
+
+    const [selectedIndex, setSelectedIndex] = useState(-1)
+
+    // items = [];
+
+    return (
+    <>
+      <h1>Heading</h1>
+      {items.length === 0 && <p>No items found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li 
+            className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
+            key={item}
+            onClick={() => {setSelectedIndex (index); 
+                            onSelectItem(item);}}
+          >  
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
