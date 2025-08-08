801ea941bdc6f68d9ff242865f77306ce8ba8160
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-08-08 22:30:42 +0530

    Update App.js

--- Day-3/App.js
+++ Day-3/App.js
@@ -2,7 +2,12 @@ import './App.css';
 
 // JSX: JSX is basicly HTML itself
 // But some of the words are reserved in JSX like for, class cannot be used
+// Babel compiles the entire JSX
+
+
 let name = "Veeresh";
+const element = <h1>Hello, {name}</h1>;
+console.log(element)
 function App() {
   return (
     <>
