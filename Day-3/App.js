7987ed70af5789cb586ffdc545099e6ea692aa2e
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-08-08 22:20:55 +0530

    Create App.js

---
+++ Day-3/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+// JSX: JSX is basicly HTML itself
+// But some of the words are reserved in JSX like for, class cannot be used
+let name = "Veeresh";
+function App() {
+  return (
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <h1>Hello {name}</h1>
+    <div className="container">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus sint tempore mollitia vitae est, dignissimos asperiores totam eius a dicta dolor at modi id obcaecati aliquam, laborum earum ipsam ea architecto deleniti nisi. Veniam?</div>
+    </>
+  );
+}
+
+export default App;
