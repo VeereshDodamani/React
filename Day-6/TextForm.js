08bb31430dcec7f25eb2dd6d3753039457c064b4
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-08-12 07:11:06 +0530

    Update TextForm.js

--- Day-6/TextForm.js
+++ Day-6/TextForm.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TextForm(props) {
+    const handelUpClick = ()=>{
+          console.log("Submit clicked")
+          setText("Clicked on handleupCase")
+    }
+    const handleOnChange = (event)=>{
+          console.log("OnChange")
+          setText(event.target.value)
+    }
+
+
+  const [text, setText] = useState('Enter text here...');
+  
   return (
-      <div class="mb-3">
-        <h1>{props.heading}</h1>
-        <textarea
-          class="form-control"
-          id="myBox"
-          rows="8"
-        ></textarea>
+    <div>
+      <h1>{props.heading}</h1>
+      <div className="mb-3">
+        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
       </div>
+      <button className="btn btn-primary" onClick={handelUpClick}>Submit</button>
+    </div>
   );
 }
