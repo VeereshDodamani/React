2e54e0793c2d8ff09f3a6147df789066e3f4938f
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-08-12 06:45:35 +0530

    Create App.js

---
+++ Day-6/App.js
@@ -0,0 +1,27 @@
+// import './App.css';
+
+import Navbar from "./Components/Navbar";
+import TextForm from "./Components/TextForm";
+
+// JSX: JSX is basicly HTML itself
+// But some of the words are reserved in JSX like for, class cannot be used
+// Babel compiles the entire JSX
+
+
+let name = "Veeresh";
+const element = <h1>Hello, {name}</h1>;
+console.log(element)
+function App() {
+  return (
+    <>
+      {/* <Navbar title = "TextUtils" aboutText="About"/> */}
+      {/* <Navbar/> */}
+      <Navbar title = "TextUtils"/>
+      <div className="container my-3">
+      <TextForm heading="Enter your heading here..."/>
+      </div>
+    </>
+  );
+}
+
+export default App;
