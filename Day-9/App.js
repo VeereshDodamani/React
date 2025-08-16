ea12daa3aaa7b117c2394cd7a833b5aa7fa4d553
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-08-16 18:32:19 +0530

    Create App.js

---
+++ Day-9/App.js
@@ -0,0 +1,40 @@
+// import './App.css';
+
+import { useState } from "react";
+import About from "./Components/About";
+import Navbar from "./Components/Navbar";
+import TextForm from "./Components/TextForm";
+
+// JSX: JSX is basicly HTML itself
+// But some of the words are reserved in JSX like for, class cannot be used
+// Babel compiles the entire JSX
+
+
+let name = "Veeresh";
+const element = <h1>Hello, {name}</h1>;
+console.log(element)
+function App() {
+  const [mode, setmode] = useState('dark')
+
+  const toggleMode =() => {
+    if (mode === 'dark'){
+      setmode('light');
+    }
+    else{
+      setmode('dark');
+    }
+  }
+  return (
+    <>
+      {/* <Navbar title = "TextUtils" aboutText="About"/> */}
+      {/* <Navbar/> */}
+      <Navbar title = "TextUtils" mode={mode} toggleMode={toggleMode}/>
+      <div className="container my-3">
+      <TextForm heading="Enter your heading here..."/>
+      </div>
+      <About></About>
+    </>
+  );
+}
+
+export default App;
