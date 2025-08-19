cefebe413fce6f7001864fe53499e38363aa4456
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-08-19 07:34:11 +0530

    Create App.js

---
+++ Day-10/App.js
@@ -0,0 +1,53 @@
+// import './App.css';
+
+import { useState } from "react";
+// import About from "./Components/About";
+import Navbar from "./Components/Navbar";
+import TextForm from "./Components/TextForm";
+import Alert from "./Components/Alert";
+ 
+// JSX: JSX is basicly HTML itself
+// But some of the words are reserved in JSX like for, class cannot be used
+// Babel compiles the entire JSX
+
+
+let name = "Veeresh";
+const element = <h1>Hello, {name}</h1>;
+console.log(element)
+function App() {
+  const [mode, setmode] = useState('white')
+  const [alert, setalert] = useState(null)
+
+  const showalert = (message, type) =>{
+    setalert({
+      msg : message,
+      type : type 
+    })
+  }
+  const toggleMode =() => {
+    if (mode === 'dark'){
+      setmode('light');
+      document.body.style.backgroundColor= 'white';
+      showalert("Light  Mode Activated....","Success");
+    }
+    else{
+      setmode('dark');
+      document.body.style.backgroundColor = 'grey';
+      showalert("Dark Mode Activated....","Success");
+    }
+  }
+  return (
+    <>
+      {/* <Navbar title = "TextUtils" aboutText="About"/> */}
+      {/* <Navbar/> */}
+      <Navbar title = "TextUtils" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
+      <div className="container my-3">
+      <TextForm heading="Enter your heading here..." mode={mode}/>
+      </div>
+      {/* <About mode={mode}></About> */}
+    </>
+  );
+}
+
+export default App;
